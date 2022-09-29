7508c416a61be623cc0afc68560d05408da5c471
simmeringstew <dev.95a2e@simplelogin.com>
2022-09-29 12:09:05 -0400

    Completed excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    let c = ((temp - 32) * 5) / 9;
+    let roundedC = Math.round(c * 10) / 10;
+    return roundedC; 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let f = (temp * 1.8) + 32;
+    let roundedF = Math.round(f * 10) / 10;
+    return roundedF;
 };
 
 // Do not edit below this line
