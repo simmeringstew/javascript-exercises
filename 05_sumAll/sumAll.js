f6fe435bffc4efce56bfb116ff5bd444a94c3fcf
simmeringstew <dev.95a2e@simplelogin.com>
2022-09-29 11:55:46 -0400

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    let smallestNum = 0;
+    let biggerNum = 0;
+
+    if (num1 > num2) {
+        smallestNum = num2;
+        biggerNum = num1;
+    }
+    else {
+        smallestNum = num1;
+        biggerNum = num2;
+    }
+    
+    let total = 0;
+
+    for (smallestNum; smallestNum <= biggerNum; smallestNum++) {
+        total += smallestNum;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
