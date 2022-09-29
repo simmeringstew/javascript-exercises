b1be83c6691c4cd6931ac1e3ff1d641f2022acae
simmeringstew <dev.95a2e@simplelogin.com>
2022-09-29 11:38:27 -0400

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
+    
+    let argv = [];
 
-};
+    for (let i = 1; i < arguments.length; i++){
+        argv.push(arguments[i]);
+    }
 
+    const copyArray = array.slice(0);
+    let newArray = [];
+
+    for (let i = 0; i < copyArray.length; i++){
+        if (argv.includes(copyArray[i])) {
+            continue;
+        }
+        else {
+            newArray.push(copyArray[i]);
+        }
+    }
+    return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
