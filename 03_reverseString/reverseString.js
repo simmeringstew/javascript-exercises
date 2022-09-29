356a5c09ddeb142f9cff535fd55a8ae2c5cbaa16
simmeringstew <dev.95a2e@simplelogin.com>
2022-09-29 09:49:01 -0400

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedArray = splitString.reverse();
+    let joinedArray = reversedArray.join("");
+    return joinedArray;
 };
 
 // Do not edit below this line
