e03a1c08157867034691b63515f7203652d73034
simmeringstew <dev.95a2e@simplelogin.com>
2022-09-29 15:39:38 -0400

    Changed for loop to a forEach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -9,14 +9,11 @@ const removeFromArray = function(array, arg) {
     const copyArray = array.slice(0);
     let newArray = [];
 
-    for (let i = 0; i < copyArray.length; i++){
-        if (argv.includes(copyArray[i])) {
-            continue;
+    copyArray.forEach(element => {
+        if (argv.includes(element) === false) {
+            newArray.push(element)
         }
-        else {
-            newArray.push(copyArray[i]);
-        }
-    }
+    });
     return newArray;
 }
 // Do not edit below this line
