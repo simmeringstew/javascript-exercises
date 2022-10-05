8a9f3f8178e19155ca4d6518e13c38745cb7fb8f
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-05 12:16:00 -0400

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	if (numbers.length === 0) {
+    return 0;
+  }
+  else {
+    const sum = numbers.reduce((total, number) => {
+      return total + number
+    }, 0);
+    return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+  else {
+    const sum = numbers.reduce((total, number) => {
+      return total * number
+    }, 1);
+    return sum;
+  }
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) {
+    return 1;
+  }
+  else {
+    let total = 1;
+    for (n; n > 1; n--) {
+      total *= n;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
