61a14a2708cfb13e1d0fe751ceec3ce3e56389c2
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-05 12:45:22 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/\W/g, "");
+    string = string.toLowerCase();
+    const splitString = string.split("");
+    const reversedString = splitString.reverse();
+    const joinedString = reversedString.join("");
+    if (joinedString === string) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
