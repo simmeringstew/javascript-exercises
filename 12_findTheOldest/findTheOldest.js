c3bc1f6ddd1bb31dc6f8739601a06d014d446297
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-05 14:21:10 -0400

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const currentYear = (new Date()).getFullYear();
+
+    people.forEach(function (person) {
+        if (!("yearOfDeath" in person)) {
+            person.yearOfDeath = currentYear;
+        }
+    });
+
+    const oldest = people.sort(function(a, b) {
+        const yearsOne = a.yearOfDeath - a.yearOfBirth;
+        const yearsTwo = b.yearOfDeath - b.yearOfBirth;
+        if (yearsOne < yearsTwo) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
