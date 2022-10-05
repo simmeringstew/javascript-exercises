69df568298cf33e8233e5d85e13791320e93ab19
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-05 13:58:53 -0400

    Finished excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    if (index < 0) {
+        return "OOPS";
+    }
+
+    let sequence = [1, 1];
+    let previous = 1;
+    let furtherPrevious = 1;
+    for (let i = 2; i < index; i++) {
+        sequence.push(furtherPrevious + previous);
+        let temp = previous;
+        previous = furtherPrevious + previous;
+        furtherPrevious = temp;
+    }
+    return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
